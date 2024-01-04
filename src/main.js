0eb80c46d8b6d868ccd721c726a035fb3be82b7a
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-01-04 13:18:27 +0200

    2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // first page
 const firstPage = {
     page: document.querySelector('.first-page'),
+    pageSecond: document.querySelector('.second-page'),
     btn: document.querySelector('.first-page-btn'),
     opacityPage:()=>{
         firstPage.page.classList.add('is-opacity');
         setTimeout(()=> {
             firstPage.page.style.display = 'none'
         }
-, 400)
+, 400);
+       setTimeout(()=>{
+         firstPage.pageSecond.classList.toggle('is-opacity')
+       },500)
     }
 }
 firstPage.btn.addEventListener('click', firstPage.opacityPage)
